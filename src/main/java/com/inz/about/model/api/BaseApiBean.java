608191e1bd261a3c245ff028656227315123159b
zhenglj <1165469346@qq.com>
2020-03-06 09:39:28 +0800

    提交旧代码
    Signed-off-by: zhenglj <1165469346@qq.com>

---
+++ src/main/java/com/inz/about/model/api/BaseApiBean.java
@@ -0,0 +1,9 @@
+package com.inz.about.model.api;
+
+/**
+ * @author inz
+ * @version 1.0.0
+ * Create by Zhenglj on 2019/05/28 14:46
+ **/
+public class ApiBaseBean {
+}
---
+++ src/main/java/com/inz/about/util/ApiUtil.java
@@ -0,0 +1,9 @@
+package com.inz.about.util;
+
+/**
+ * @author inz
+ * @version 1.0.0
+ * Create by Zhenglj on 2019/05/28 13:45
+ **/
+public class ApiUtil {
+}
